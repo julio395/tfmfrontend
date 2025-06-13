520750f1d42618b2d86494202e52eceb08c5c741
julio395 <juliocole1@gmail.com>
2025-06-13 19:24:53 +0200

    nuevo cambio 6

--- src/appwrite/appwrite.js
+++ src/appwrite/appwrite.js
@@ -173,7 +173,8 @@ export const getUsers = async () => {
             headers: {
                 'X-Appwrite-Project': client.config.project,
                 'X-Appwrite-Key': process.env.REACT_APP_APPWRITE_API_KEY,
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'X-Appwrite-Response-Format': '1.0.0'
             }
         });
 
